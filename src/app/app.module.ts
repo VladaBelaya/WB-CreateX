5da0e58392c4459ffb7decab9f321f154c12c2ef
VladaBelaya <vlada_chvanova@mail.ru>
2022-11-09 23:47:39 +0300

    create files styles(blocks), create new component Sign In

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
