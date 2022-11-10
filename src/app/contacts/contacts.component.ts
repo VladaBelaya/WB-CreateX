17f7620e33ed4f0e0b9bee01e6ce60d19910528f
VladaBelaya <vlada_chvanova@mail.ru>
2022-11-10 19:26:41 +0300

    fix. svg in assets folder. correct styles. create new variables in component  and inserting them into the template

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-
+interface ContactInfo {
+  mail: string,
+  phone: string,
+  address: string
+}
+interface MapConfig {
+  centerMap: number[],
+  zoom: number,
+  markGeometry: number[]
+}
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
@@ -8,6 +17,17 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ContactsComponent implements OnInit {
+  public readonly contactInfo: ContactInfo = {
+    mail: 'hello@createx.com',
+    phone: '(405) 555-0128',
+    address: '2464 Royal Ln. Mesa, New Jersey 45463, USA'
+  }
+
+  public readonly mapConfig: MapConfig = {
+    centerMap: [55.751952, 37.600739],
+    zoom: 12,
+    markGeometry: [55.751952, 37.600739]
+  }
 
   ngOnInit() {
   }
